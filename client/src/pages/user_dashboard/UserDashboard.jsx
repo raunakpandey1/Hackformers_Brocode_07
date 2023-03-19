e58ac4acd91f40669d91d8f8c0a52cd413c6e239
Sushil kumar Gupta <skgupta77159@gmail.com>
2023-03-19 08:15:30 +0530

    remove user dashboard comment

--- client/src/pages/user_dashboard/UserDashboard.jsx
+++ client/src/pages/user_dashboard/UserDashboard.jsx
@@ -4,6 +4,7 @@ import "./userdashboard.css";
 import axios from "../../axios";
 import { AppContext } from "../../context/AppContext";
 import { ethers } from "ethers";
+import RefreshIcon from "../../static/svg/RefreshIcon";
 
 export default function UserDashboard({
   provider,
@@ -16,61 +17,22 @@ export default function UserDashboard({
 
   const Getdata = async () => {
     let dataArray;
-    // console.log("helo", complaintContract, account)
     try {
-        console.log(account)
-       dataArray = await complaintContract.displayComplaint(account);
-
-      //   setData(dataArray)
-
-      //   await dataArray.forEach(async (e) => {
-      //     await fetch(e).then(async (response) => {
-      //         let ob = await response.json()
-      //         // console.log(ob)
-      //         // newArr.push(ob)
-      //         setData((dat) => [...dat, ob]);
-      //     });
-      //     // setData((dat) => [...dat, e]);
-      //   });
+      dataArray = await complaintContract.displayComplaint(account);
       dataArray.forEach(async (e) => {
-        // let dat1 = await axios.get(e);
         let dat1 = await axios.get(e);
-          newArr.push(dat1.data);
-        // setData([...data,  dat1.data]);
-        // console.log(typeof dat1.data)
+        newArr.push(dat1.data);
+        Getdata1()
       });
-
-      //   console.log(newArr);
-      //   setData(newArr)
     } catch (e) {
-    //   console.log("Helo", e)
       alert("You don't have access");
     }
-    // const isEmpty = Object.keys(dataArray).length === 0;
-
-    // if (!isEmpty) {
-
-    //   const str = dataArray.toString();
-    //   const str_array = str.split(",");
-    //   // console.log(str);
-    //   console.log(str_array);
-    //   const images = str_array.map(async(item, i) => {
-    //     let dat1 = await axios.get(item);
-    //     // setData([...data,  dat1.data]);
-
-    //   });
-    //   console.log(images)
-    //   setData([...images]);
-    // } else {
-    //   alert("No image to display");
-    // }
-    //   console.log(dataArray)
   };
+
   const Getdata1 = () => {
     setData(newArr)
     console.log(newArr);
   };
-  console.log(data)
 
   const rewardUser = async () => {
     const amount = { value: ethers.utils.parseEther("0.005") };
@@ -79,7 +41,6 @@ export default function UserDashboard({
     console.log("Transaction is done");
   }
 
-  console.log(newArr)
   return (
     <div className="policeDashboard">
       <div className="pdWrapper">
@@ -102,9 +63,9 @@ export default function UserDashboard({
         </div>
 
         <div className="createComplaints">
-          {/* <button className="createComBtn">
-                        +
-                    </button> */}
+          <button className="refreshBtn" onClick={Getdata}>
+            <RefreshIcon />
+          </button>
           <CreateComplaintForm
             signal={true}
             provider={provider}
@@ -115,14 +76,6 @@ export default function UserDashboard({
         </div>
 
         <div className="pdComplaints">
-          <button className="primary" onClick={Getdata}>
-            Get url
-          </button>{" "}
-          <button className="primary" onClick={Getdata1}>
-            Get Data
-          </button>
-          {/* <div className="image-list">{newArr}</div>
-          {console.log(data.length)} */}
           {data.length > 0 &&
             data.map((e) => {
               return (
@@ -135,37 +88,10 @@ export default function UserDashboard({
 
                     <p>{e.description}</p>
                     <h3>{e.walletAddress}</h3>
-                    {/* <select value={e.status}>
-                      <option value="Action">Action</option>
-                      <option value="Accept">Accept</option>
-                      <option value="Reject">Reject</option>
-                    </select>
-                    <button className="rewardBtn" onClick={rewardUser}>Reward</button> */}
                   </div>
                 </div>
               );
             })}
-          {/* <div className="pComplaintDiv">
-            <div className="pcdLeft">
-              <img src="https://i.ndtvimg.com/mt/2014-12/indian_traffic_generic_thinkstock_650_bigstry.jpg" />
-            </div>
-            <div className="pcdRight">
-              <h3>Pollution due to daily road rage</h3>
-              <span>18/03/2023 16:25</span>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-                rerum reprehenderit perspiciatis dignissimos magnam, quis minima
-                repellat sapiente sunt, eos ea? Assumenda perspiciatis beatae
-                sit aliquam laboriosam alias ipsum corrupti!
-              </p>
-              <select>
-                <option value="Action">Action</option>
-                <option value="Accept">Accept</option>
-                <option value="Reject">Reject</option>
-              </select>
-              <button className="rewardBtn">Reward</button>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
